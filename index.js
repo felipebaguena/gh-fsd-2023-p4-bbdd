eb0375ad4a2e47e095c3210eab83a00e190ac9b6
Alyna Nastas Romaniuc <nastasalyna@gmail.com>
2023-03-02 16:06:19 +0100

    feat:Create DB

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/welcome', (req, res) => {
     return res.send("Bienvenido a mi app")
 })
 
-// const router = require('./router');
-// app.use(router);
+//const router = require('./router');
+//app.use(router);
 
 db.then(() => {
-    //Starting server
+
     app.listen(PORT, () => console.log("Server on port " + PORT));
 })
-    .catch((error) => console.log(error.message));   
+    .catch((err) => console.log(err.message));   
