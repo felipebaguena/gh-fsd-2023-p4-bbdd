3eeed47b26f4ad8556eb01301e9b22374a03c12f
Alyna Nastas Romaniuc <nastasalyna@gmail.com>
2023-03-02 17:53:49 +0100

    feat:Doctor, Intervention, speciality, appointment migration created

---
+++ migrations/20230302165213-create-appointment.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      doctor_id: {
+        type: Sequelize.INTEGER
+      },
+      patient_id: {
+        type: Sequelize.INTEGER
+      },
+      intervention_id: {
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      comments: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Appointments');
+  }
+};
