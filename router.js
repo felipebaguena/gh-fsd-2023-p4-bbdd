ad3d93cb3c21f5f8877ec36267cc289ce676daae
felipebaguena <felipebaguena@gmail.com>
2023-03-02 20:16:36 +0100

    feat:userrouts in router

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
 const roleRoutes = require('./views/roleRoutes');
+const userRoutes = require('./views/userRoutes');
 
 
 
 router.use('/', roleRoutes)
-
+router.use('/', userRoutes);
 
 
 
