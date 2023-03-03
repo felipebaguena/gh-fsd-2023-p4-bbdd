4b615fe85cac53852e90564a2fa1c8075f5f259c
Alyna Nastas Romaniuc <nastasalyna@gmail.com>
2023-03-03 15:51:44 +0100

    feat: verify token middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,23 @@
+const jwt = require ('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    try { 
+        const authorization = req.headers.authorization;
+
+        if (!authorization) {
+            return res.send('Invalid token');
+        }
+        const [ strategy, token ] = authorization.split(" ");
+
+        const decoded = jwt.verify(token, 'secreto');
+
+        req.userId = decoded.userId;
+        req.roles = decoded.roles; 
+        
+        next();
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+module.exports = verifyToken;
