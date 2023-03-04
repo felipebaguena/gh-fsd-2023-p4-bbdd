ad96821a3bf745e8cbe55b566024cbda664b8a7d
felipebaguena <felipebaguena@gmail.com>
2023-03-04 19:48:26 +0100

    fix:appoint rout

--- views/appointRoutes.js
+++ views/appointRoutes.js
@@ -9,8 +9,6 @@ router.post('/appoint', verifyToken, appointController.createAppoint)
 router.put('/appoint', verifyToken, appointController.updateAppointment)
 router.delete('/appoint/:id', verifyToken, appointController.deleteAppointment)
 router.get('/admin/appoint', verifyToken, isAdmin, appointController.getAllAppointments)
-
-
-router.get('/appoint/comming',verifyToken,appointController.getUpcomingAppointments)
+router.get('/appoint/coming',verifyToken,appointController.getUpcomingAppointments)
 
 module.exports = router;
