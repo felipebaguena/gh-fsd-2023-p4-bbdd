88bbb481fb255fe9723833c6fabd48243be69289
felipebaguena <felipebaguena@gmail.com>
2023-03-04 09:02:52 +0100

    feat: create appointment controller

--- controllers/appointController.js
+++ controllers/appointController.js
@@ -1,7 +1,26 @@
+const {Appointment} = require('../models');
+
 const appointController = {}
 
 appointController.getAppoint = (req, res) => {
-  res.status(200).send('Todo está correcto');
+    res.status(200).send('Todo está correcto');
 }
+
+appointController.createAppoint = async(req, res) => {
+    try {
+        const { doctor_id, patient_id, intervention_id, date, comments } = req.body;
+        const newAppointment = {
+            doctor_id,
+            patient_id,
+            intervention_id,
+            date,
+            comments
+        }
+        const appointment = await Appointment.create(newAppointment)
+        return res.json(appointment)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+};
 
 module.exports = appointController;
--- views/appointRoutes.js
+++ views/appointRoutes.js
@@ -3,5 +3,6 @@ const appointController = require('../controllers/appointController')
 const router = require ('express').Router();
 
 router.get('/appoint',appointController.getAppoint)
+router.post('/appoint',appointController.createAppoint)
 
 module.exports = router;
