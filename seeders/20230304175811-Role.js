15dcae2cf9c2b74a8482202324c4a3e3fe066679
Alyna Nastas Romaniuc <nastasalyna@gmail.com>
2023-03-05 12:38:23 +0100

    fix: language translation seeders

--- seeders/20230304175811-Role.js
+++ seeders/20230304175811-Role.js
@@ -7,7 +7,7 @@ module.exports = {
     [
         {id: 1, privilege: "Admin", updatedAt: "2023-03-04", createdAt: "2023-01-12"},
         {id: 2, privilege: "Doctor",updatedAt: "2023-03-04", createdAt: "2023-02-21"},
-        {id: 3, privilege: "User", updatedAt: "2023-03-04", createdAt: "2023-03-34"},
+        {id: 3, privilege: "User", updatedAt: "2023-03-04", createdAt: "2023-03-04"},
   
   
     ], {});
--- seeders/20230305094608-Appointment.js
+++ seeders/20230305094608-Appointment.js
@@ -8,7 +8,7 @@ module.exports = {
     
       {id: 1, doctor_id: 1, patient_id: 4, intervention_id: 3,  updatedAt: "2023-01-04", createdAt: "2023-01-12"},
       {id: 2, doctor_id: 2, patient_id: 5, intervention_id: 2,  updatedAt: "2023-02-05", createdAt: "2023-01-12"},
-      {id: 3, doctor_id: 2, patient_id: 6, intervention_id: 1,  updatedAt: "2023-03-06", createdAt: "2023-01-12"},
+      {id: 3, doctor_id: 2, patient_id: 6, intervention_id: 1,  updatedAt: "2023-03-06", createdAt: "2023-01-12"}
 
     ], {});
   },
