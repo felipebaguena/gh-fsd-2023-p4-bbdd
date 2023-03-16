f96e26c3d789f7331e9b8e4c874a4d59332b751e
felipebaguena <felipebaguena@gmail.com>
2023-03-16 17:33:08 +0100

    feat: add cors

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require ("cors");
+
+
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
 
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 const db = require('./db');
 
